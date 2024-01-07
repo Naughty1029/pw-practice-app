1e50ce34ee541de689c1194ed8dbbe50596581ec
渡邉雅広 <watanabemasahiro@masahiro.local>
2024-01-07 10:41:26 +0900

    Locator syntax rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -3,36 +3,25 @@ import { test } from "@playwright/test";
 //全体に対するhooks
 test.beforeEach(async({page})=>{
   await page.goto("http://localhost:4200/")
+  await page.getByText("Forms").click()
+  await page.getByText("Form Layouts").click()
 })
 
-//グループ化
-test.describe.only("suite1", ()=>{
-  //グループ内に対するhooks
-  test.beforeEach(async({page})=>{
-    await page.getByText("Charts").click()
-  })
-
-  test("the first test", async({page})=>{
-    await page.getByText("Form Layouts").click()
-  })
-  
-  test("navigate to datepicker page", async({page})=>{
-    await page.getByText("Datepicker").click()
-  })
-})
-
-//グループ化
-test.describe("suite1", ()=>{
-  //グループ内に対するhooks
-  test.beforeEach(async({page})=>{
-    await page.getByText("Forms").click()
-  })
-
-  test("the first test1", async({page})=>{
-    await page.getByText("Form Layouts").click()
-  })
-  
-  test("navigate to datepicker page2", async({page})=>{
-    await page.getByText("Datepicker").click()
-  })
+test("Locator syntax rules",async({page})=>{
+  //by tag name
+  await page.locator("input").first().click();
+  //by ID
+  await page.locator("#inputEmail1").click();
+  //by Class Value
+  page.locator(".shape-rectangle");
+  //by attribute
+  page.locator('[placeholder="Email"]')
+  //by Class Value(full)
+  page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+  //Combine
+  page.locator('input[placeholder="Email"]');
+  //by partial text message
+  page.locator(':text("Using")')
+  //by exact text message
+  page.locator(':text-is("Using the Grid")')
 })
