04816b5da6289fdd7d51464bd729589e9c959dc2
渡邉雅広 <watanabemasahiro@masahiro.local>
2024-01-07 14:24:39 +0900

    locationg parent elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -49,3 +49,16 @@ test("locationg child elements",async({page})=>{
   //indexを指定することはなるべく避ける
   await page.locator('nb-card').nth(3).getByRole('button').click();
 })
+
+test("locationg parent elements",async({page})=>{
+  await page.locator('nb-card', {hasText:'Using the Grid'}).getByRole('textbox',{name:"Email"}).click();
+  await page.locator('nb-card', {has:page.locator('#inputEmail1')}).getByRole('textbox',{name:"Email"}).click();
+
+  await page.locator('nb-card').filter( {hasText:'Basic form'}).getByRole('textbox',{name:"Email"}).click();
+  await page.locator('nb-card').filter( {has:page.locator('.status-danger')}).getByRole('textbox',{name:"Password"}).click();
+
+  await page.locator('nb-card').filter( {has:page.locator('nb-checkbox')}).filter( {hasText:'Sign in'})
+    .getByRole('textbox',{name:"Email"}).click();
+
+  await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox',{name:"Email"}).click();
+})
